3bcb0cc86d19b508da7e3f6143b797bf894f58a1
fanpeng <peng.fan@ukelink.com>
2025-12-25 18:39:47 +0800

    save

--- ZLTabBarExtension/Classes/ZLTabBarExtension.h
+++ ZLTabBarExtension/Classes/ZLTabBarExtension.h
@@ -1,16 +1,4 @@
-//
-//  ZLTabBarExtension.h
-//  ZLTabBarExtension
-//
-//  Created by admin on 2025/12/25.
-//
 
-#import <Foundation/Foundation.h>
+
 #import "UITabBar+ZLExtension.h"
-NS_ASSUME_NONNULL_BEGIN
 
-@interface ZLTabBarExtension : NSObject
-
-@end
-
-NS_ASSUME_NONNULL_END
